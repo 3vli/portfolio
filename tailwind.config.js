70d6180786987d891ab4b109ca92a7a25b53e5c5
3vli <om1ak2345@gmail.com>
2024-12-02 22:51:33 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./*.html", "./src/script/*.js"],
+  theme: {
+    
+    extend: {
+      backgroundImage: {
+
+      },
+  colors: {
+    'nav-color': '#211F23',
+    'bg-color': '#0C0C0D',
+    'js': '#C3C99E',
+    'react': '#7D9CA5',
+    'next': '#C2C2C2'
+
+    },
+      width: {
+        'w-400': '250px',
+      
+      },
+      height: {
+        '277': '150px',
+      }
+    },
+  },
+  plugins: [],
+}
